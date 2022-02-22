2f40cc1c9cf884219bd96cc8f19274b4bcceb255
Eomy <hmu1212@hotmail.com>
2022-02-22 18:24:32 +0900

    [feat] fetch post and users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,21 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const ids = new Set();
+  getState().posts.forEach((post) => ids.add(post.userId));
+  ids.forEach((id) => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
