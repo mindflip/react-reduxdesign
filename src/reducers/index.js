af5b3c491039884aee710743a0df40ca3d0724bf
Eomy <hmu1212@hotmail.com>
2022-02-22 17:20:14 +0900

    [feat] reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,5 @@
 import { combineReducers } from "redux";
-export default combineReducers({});
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
+
+export default combineReducers({ posts: postsReducer, users: usersReducer });
